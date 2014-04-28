6d98710a1127aedba83ba62b2cb6efe9106f625b
Vivek Bhagat <vivekbhagat.1990@gmail.com>
2014-04-28 14:14:53 +0530

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,3 +1,5 @@
+## cacheSolve calculates the inverse of a matrix for the first time or retrieves it from cache
+## if the values have already been parsed into the previous function
 cacheSolve <- function(x, ...)
 {
         s <- x$getsolve()
